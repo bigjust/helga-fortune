76899a9b66c12d885576de75158b573e0bd16368
Justin Caratzas <bigjust@lambdaphil.es>
2014-11-23 12:20:59 -0500

    allow fortune command to specify fortune file

--- helga_fortune.py
+++ helga_fortune.py
@@ -4,4 +4,8 @@ from helga.plugins import command
 
 @command('fortune', help='provide a fortune')
 def fortune (client, channel, nick, message, cmd, args):
-    return subprocess.check_output(['fortune', '-s'])
+    if args:
+        fortune_file = args[0]
+    else:
+        fortune_file = ''
+    return subprocess.check_output(['fortune', '-s', fortune_file])
